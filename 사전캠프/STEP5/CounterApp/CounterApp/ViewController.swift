bf95b94ac710c8a738ae830e389e5c1b2f41af3e
이민재 <qlqlzbto@naver.com>
2025-02-18 16:58:09 +0900

    프로젝트 생성

---
+++ 사전캠프/STEP5/CounterApp/CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by 이민재 on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
