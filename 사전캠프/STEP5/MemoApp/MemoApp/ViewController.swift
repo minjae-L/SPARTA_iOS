738e957cfb6e1e1dac89a3dbd8dca65676824756
이민재 <qlqlzbto@naver.com>
2025-02-27 02:24:34 +0900

    Set UI NavigationController, Tabbar, TableView

--- 사전캠프/STEP5/MemoApp/MemoApp/ViewController.swift
+++ 사전캠프/STEP5/MemoApp/MemoApp/ViewController.swift
@@ -7,13 +7,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .cyan
+enum Constant {
+    static var ADD_BUTTON_NAME: String {
+        return "square.and.pencil"
     }
-
-
+    static var TAB_BAR_HEIGHT: CGFloat {
+        return 80
+    }
 }
 
+class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tb = UITableView()
+        tb.delegate = self
+        tb.dataSource = self
+        tb.translatesAutoresizingMaskIntoConstraints = false
+        return tb
+    }()
+    
+    private lazy var tabbar: UITabBar = {
+        let tbb = UITabBar()
+        tbb.frame = .zero
+        tbb.backgroundColor = .red
+        tbb.translatesAutoresizingMaskIntoConstraints = false
+        return tbb
+    }()
+    
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        super.viewDidLoad()
+        addViews()
+        configureLayout()
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = "메모"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: Constant.ADD_BUTTON_NAME),
+                                                                 style: .done,
+                                                                 target: self,
+                                                                 action: #selector(addButtonTapped))
+    }
+    
+    @objc func addButtonTapped() {
+        print("add")
+    }
+    
+    private func addViews() {
+        view.addSubview(tableView)
+        view.addSubview(tabbar)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            tabbar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tabbar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tabbar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tabbar.heightAnchor.constraint(equalToConstant: Constant.TAB_BAR_HEIGHT),
+            tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            
+        ])
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
