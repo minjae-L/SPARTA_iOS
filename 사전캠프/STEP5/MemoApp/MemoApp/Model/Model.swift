415f8382bb9f68a361cc41fbda7295d503395738
이민재 <qlqlzbto@naver.com>
2025-02-27 12:34:43 +0900

    model, viewModel 생성

---
+++ 사전캠프/STEP5/MemoApp/MemoApp/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MemoApp
+//
+//  Created by 이민재 on 2/27/25.
+//
+
+import Foundation
---
+++ 사전캠프/STEP5/MemoApp/MemoApp/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  MemoApp
+//
+//  Created by 이민재 on 2/27/25.
+//
+
+import Foundation
